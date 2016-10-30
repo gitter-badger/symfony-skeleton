2446f41cf893cd2fb86dad53af0ec61338aa5556
Miguel Ángel Martín <miguelbemartin@gmail.com>
2016-10-30 23:31:38 +0100

    Added tests

--- tests/AppBundle/Controller/LandingControllerTest.php
+++ tests/AppBundle/Controller/LandingControllerTest.php
@@ -15,4 +15,35 @@ class LandingControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Symfony Skeleton', $crawler->filter('a')->text());
     }
+
+    /** @dataProvider provideUrls */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function provideUrls()
+    {
+        return [
+            array('/'),
+            array('/login'),
+            array('/register//'),
+        ];
+    }
+
+    /** @test */
+    public function timeToLoadHomepage()
+    {
+        $client = static::createClient();
+        $client->enableProfiler();
+        $client->request('GET', '/');
+        if ($profiler = $client->getProfile()) {
+            $this->assertLessThan(
+                500,
+                $profiler->getCollector('time')->getDuration()
+            );
+        }
+    }
 }
